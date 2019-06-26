df08b84cee17c5cad17399b56b821dde7eb69dfe
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-06-26 05:24:18 +0500

    add extension to loopy rhymes

--- loopy_rhymes.py
+++ loopy_rhymes.py
@@ -20,3 +20,37 @@ def littleMonkeys(numMonkeys):
 song = littleMonkeys(10)
 print(song)
 
+
+## further extension
+
+def wrong_list_entries(lst):
+    invalid_input = False
+    for value in lst:
+        if type(value) != str:
+            invalid_input = True
+            break
+    return invalid_input
+
+
+def OldMacDonald(sounds, animals):
+    error_message = "Please give to list of strings with equal number of items!"
+    if type(sounds) != list or type(animals) != list:
+        return error_message
+    elif wrong_list_entries(sounds) or wrong_list_entries(animals):
+        return error_message
+    elif len(sounds) != len(animals):
+        return error_message
+    else:
+        for i in range(len(sounds)):
+            print("Old Macdonald had a farm E-I-E-I-O")
+            print("And on his farm he had a " + animals[i] + " E-I-E-I-O")
+            print("With a " + sounds[i] + "-" + sounds[i] + " here and a " + sounds[i] + \
+                    "-" + sounds[i] + " there")
+            print("Here a " + sounds[i] + " there a " + sounds[i] +", everywhere a " + \
+                    sounds[i] + "-" + sounds[i])
+
+sounds = ['quack', 'woof', 'meow', 'moo', 'neigh', 'baa', 'cluck']
+animals = ['duck', 'dog', 'cat', 'cow', 'horse', 'sheep', 'chicken']
+OldMacDonald(sounds, animals)
+
+
