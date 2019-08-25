4f945bdd3eb8bf02c2a9a9eb6da09024aa3b016a
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-08-25 23:51:23 +0500

    add more solutions

--- sharing_my_money.py
+++
@@ -1,9 +0,0 @@
-'''
-the following program is a solution to Challenge 13 in
-the programming workbook - Introduction to Programming
-in Python
-'''
-
-shares = int(input("How many people do you want to share the money with?\n"))
-money = 10000/shares
-print("each person would get: ", money, " rupees")
