c08f0680d5e55ddc0e1a246ccc740eb6c1088b5f
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-08-26 11:00:13 +0500

    add mad_libs

---
+++ examples_for_teachers/mad_libs.py
@@ -0,0 +1,19 @@
+# An example that teacher can use for discussion
+
+noun_1 = input("Give me a plural noun \n")
+verb = input("Give me a verb ending in ed \n")
+noun_2 = input("Give me another plural noun \n")
+noun_3 = input("Give me a noun \n")
+adjective = input("Give me an adjective \n")
+food_1 = input("Give me a type of food \n")
+food_2 = input("Give me another type of food \n")
+noun_4 = input("Give me a noun \n")
+noun_5 = input("and another one \n")
+noun_6 = input("Give me one last noun \n")
+
+print("Ice cream is a frozen dessert made from " + noun_1 + ", with added flavours and sweetners")
+print("This mixure is quickly frozen while it is " + verb + ", so that large " + noun_2 + " do not form.")
+print("Some ice cream is made with " + noun_3 + " extracted from seaweed, so that it is not " + adjective)
+print("There are many different flavours of ice cream, such as " + food_1 + " and " + food_2)
+print("Ice cream often has things added to it for flavor, like " + noun_4 + ", " + noun_5 + " or " + noun_6)
+
