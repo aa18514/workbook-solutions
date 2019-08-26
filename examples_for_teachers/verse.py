f7d3f8987acfcd2bf2e923dbe13cd9db9b431185
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-08-26 10:50:22 +0500

    add examples for teachers

---
+++ examples_for_teachers/verse.py
@@ -0,0 +1,12 @@
+# An example that teacher can use for discussion
+
+verse = """
+The wheels on the bus go round and round,
+Round and round, round and round,
+The wheels on the bus go round and round,
+All day long.
+"""
+
+new_verse = verse.replace("wheels", "horns")
+print(new_verse)
+
